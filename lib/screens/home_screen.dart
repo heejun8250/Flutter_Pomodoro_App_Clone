a5a51c909df9ae696b0e4380847c9ec1b7e645b3
heejun <gmlwns262@naver.com>
2024-03-28 17:49:07 +0900

    타이머 시작 기능 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds -= 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(Duration(seconds: 1), onTick);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                  '25:00',
+                  '$totalSeconds',
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
@@ -38,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
                   iconSize: 120,
                   color: Theme.of(context).cardColor,
-                  onPressed: () {},
+                  onPressed: onStartPressed,
                   icon: Icon(Icons.play_circle_outline),
                 ),
               ),
